913d7f43d08bf8e9f52c90885771271291e92a29
Onetechlabs <onetechlabsbymuhammadridwan@gmail.com>
2020-05-21 22:54:04 +0700

    fix migration errors

--- models/account.rb
+++ models/account.rb
@@ -5,17 +5,19 @@ class Account < Sequel::Model
   attr_accessor :password, :password_confirmation
 
   def validate
-    validates_presence     :avatar
-    validates_presence     :email
-    validates_presence     :role
-    validates_presence     :password if password_required
-    validates_presence     :password_confirmation if password_required
-    validates_length_range 4..40, :password unless password.blank?
+    validates_presence     :name, message: 'Name is Required'
+    validates_presence     :surname, message: 'Surname is Required'
+    validates_presence     :avatar, message: 'Avatar is Required'
+    validates_presence     :email, message: 'Email is Required'
+    validates_presence     :role, message: 'Role is Required'
+    validates_presence     :password, message: 'Password is Required' if password_required
+    validates_presence     :password_confirmation, message: 'Password Confirmation is Required' if password_required
+    validates_length_range 4..40, :password, message: 'Password Min 4 and Max 40' unless password.blank?
     errors.add(:password_confirmation, 'must confirm password') if !password.blank? && password != password_confirmation
-    validates_length_range 3..100, :email unless email.blank?
-    validates_unique       :email unless email.blank?
-    validates_format       /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :email unless email.blank?
-    validates_format       /[A-Za-z]/, :role unless role.blank?
+    validates_length_range 3..100, :email, message: 'Email Min 3 and Max 100' unless email.blank?
+    validates_unique       :email, message: 'Error Duplicate Email' unless email.blank?
+    validates_format       /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :email, message: 'Email is Invalid Format' unless email.blank?
+    validates_format       /[A-Za-z]/, :role, message: 'Role must an alphabet' unless role.blank?
   end
 
   # Callbacks
